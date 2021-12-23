3d0e9c4095796bc4f511fe2f3d7ce30ee7af9ba0
rezky <88895489+phantom43@users.noreply.github.com>
2021-12-23 19:29:52 +0800

    Create hoki.py

---
+++ hoki.py
@@ -0,0 +1,8 @@
+from random  import randint
+hoki = randint(0, 100)
+
+print("tingkat ke hokian anda", hoki)
+if hoki <= 50:
+	print("anda kurang beruntung")
+if hoki >= 100:
+	print("anda beruntung")
